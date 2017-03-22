d40f358cc6f1d414bbf409cc2670a60fae3dccbd
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-03-22 11:19:37 -0700

    commit: IndexOf addition

--- app.js
+++ app.js
@@ -1,59 +1,88 @@
 // Application state = object
-state = {
-     items: ["apple","oranges", "bread", "milk"], // Initial array values
-     addToShoppingList: function (item) {
-          this.items.push(item);
-     },
-     createShoppingList: function () {
-          this.items.forEach(function(item){
-               var shoppingListElement = (`
-                  <li>
-                    <span class="shopping-item">${item}</span>
-                    <div class="shopping-item-controls">
-                    <button class="shopping-item-toggle">
-                    <span class= "button-label">check</span>
-                    </button>
-                    <button class="shopping-item-delete">
-                    <span class ="button-label">delete</span>
-                    </button>
-                    </div>
-                  </li>`);
-                  if(item !=="") $('.shopping-list').append(shoppingListElement);
-             });
-     },
-     deleteButton: function () {
-          $('.shopping-list').on("click", '.shopping-item-delete', function(){
-               var listItem = $(this).closest('li');
-               var indexToRemove = listItem.index();
-               listItem.remove();
-               shoppingListState.items.splice(indexToRemove, 1);
-               });
-     },
-     purchased: function () {
-          $('.shopping-list').on("click", '.shopping-item-toggle', function(){
-               $(this).closest('li').children('.shopping-item').toggleClass('shopping-item__checked');
-               });
-     }
+var state = {
+     items: [ ],
+};
+
+function addToShoppingList (state, item){
+     state.items.push({
+          name: item,
+          checked: false
+     });
+}
+
+//state.items[].Name
+function findItemIndex(state, item) {
+     // Access the object state
+     var objArr = state.items
+	for(var i = 0; i < objArr.length; i++) {
+		if (objArr[i].name === item){
+			return i;
+		}
+	}
+}
+
+function updateStatus(state, element) {
+	// fint the item name from selected element $('.shopping-list')
+	var itemName = element.closest('li').children('.shopping-item').text();
+	//find item in state.items
+	var itemIndex = findItemIndex(state, itemName);
+	state.items[itemIndex].checked = !state.items[itemIndex].checked;
 }
 
 
-//Main Event Listeners
-$('#js-shopping-list-form').submit(function(event) {
-     event.preventDefault();
-     $('.shopping-list li').remove(); // this line prevents initial array values to append to bottom
-      var shoppingItem = $('input#shopping-list-entry').val();
-      state.addToShoppingList(shoppingItem);
-      state.createShoppingList();
-    })
-state.deleteButton();
-state.purchased();
-
-//.toggleClass doesnt stay into effect when more items are added
-
-/*
-Add alert to make sure form field is populated
-if(state.items.trim().length === 0) {
-    alert("You must enter an item in order for it to be added.");
- return;
+function deleteItem(state, element) {
+	//find item name
+	var itemName = element.closest('li').children('.shopping-item').text();
+	//find item in state.items
+	var itemIndex = findItemIndex(state, itemName);
+	// remove item from state.items
+	state.items.splice(itemIndex, 1);
 }
-*/
+
+
+function crossOutItem(element) {
+	element.closest('li').children('.shopping-item').toggleClass('shopping-item__checked');
+}
+
+//DOM Manipulation
+function renderList (state, element, item, action) {
+	if (action === 'add') {
+		var itemIndex = findItemIndex(state, item);
+		var itemsHTML =
+          '<li><span class="shopping-item">'
+          + state.items[itemIndex].name
+          + '</span><div class="shopping-item-controls">'
+          + '<button class="shopping-item-toggle">'
+          +'<span class="button-label">check</span>'
+          +'</button><button class="shopping-item-delete">'
+          +'<span class="button-label">delete</span></button></div></li>';
+		element.append(itemsHTML);
+	} else if (action === 'remove') {
+		element.closest('li').remove();
+	}
+}
+
+
+
+$('document').ready(function () {
+
+     //form submission
+	$('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    var itemName = $('#shopping-list-entry').val();
+    addToShoppingList(state, itemName);
+    renderList(state, $('.shopping-list'), itemName, 'add');
+	});
+
+     //check item toggle
+	$('.shopping-list').on('click', '.shopping-item-toggle' ,function() {
+		updateStatus(state, $(this));
+		crossOutItem($(this));
+	});
+
+     //delete items
+	$('.shopping-list').on('click', '.shopping-item-delete', function() {
+		crossOutItem(state, $(this));
+		renderList(state, $(this), null, 'remove');
+	});
+});
